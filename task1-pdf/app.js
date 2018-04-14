b6c0093a54002caeb8f6959222530d6261c29c70
MossyChuck <ipasha1975@gmail.com>
2018-04-15 01:08:58 +0300

    refactoring

--- task1-pdf/app.js
+++ task1-pdf/app.js
@@ -10,55 +10,62 @@ app.use(cors());
 
 app.get('/:firstName', (req, res) => {
   const firstName = req.params.firstName;
-  console.log(firstName);
   db.query('select * from users where firstName=?;', [firstName], (err, results, fields) => {
     if (err) {
-      console.log(err);
       res.send(false);
     } else {
       const user = results[0];
       if (!user) {
-        console.log('no such user');
         res.send(false);
       } else {
-        let image = `data:image/png;base64,${user.image.toString('ascii')}`;
-        let document = new PDFDocument();
-        let writeStream = document.pipe(fs.createWriteStream(config.pdf));
-        document.fontSize(25)
-            .text(user.firstName + ' ' + user.lastName, 20, 20)
-            .image(image, 20, 50)
-            .save();
-
-        document.end();
-
-        writeStream.on('finish', () => {
-          let readStream = fs.createReadStream(config.pdf);
-          let resBuffer;
-          readStream.on('readable', () => {
-            let buf = readStream.read();
-            if (buf) {
-              if (resBuffer) {
-                resBuffer = Buffer.concat([resBuffer, buf]);
-              } else {
-                resBuffer = buf;
-              }
-            } else {
-              db.query('update users set pdf=? where firstName=?', [resBuffer, firstName], (err, results, fields) => {
-                if (err) {
-                  console.log(err);
-                  res.send(false);
-                } else {
-                  console.log('success');
-                  res.send(true);
-                }
-              });
-            }
-          });
-        });
+        makePDF(res,user);
       }
     }
   })
 });
+
+function makePDFBuffer(res,firstName) {
+  let readStream = fs.createReadStream(config.pdf);
+  let resBuffer;
+  readStream.on('readable', () => {
+    let buf = readStream.read();
+    if (buf) {
+      if (resBuffer) {
+        resBuffer = Buffer.concat([resBuffer, buf]);
+      } else {
+        resBuffer = buf;
+      }
+    } else {
+      writePDF(res,resBuffer,firstName);
+    }
+  });
+}
+
+function makePDF(res,user) {
+  let image = `data:image/png;base64,${user.image.toString('ascii')}`;
+  let document = new PDFDocument();
+  let writeStream = document.pipe(fs.createWriteStream(config.pdf));
+  document.fontSize(25)
+      .text(user.firstName + ' ' + user.lastName, 20, 20)
+      .image(image, 20, 50)
+      .save();
+
+  document.end();
+
+  writeStream.on('finish', () => {
+    makePDFBuffer(res,user.firstName);
+  });
+}
+
+function writePDF(res,resBuffer,firstName) {
+  db.query('update users set pdf=? where firstName=?', [resBuffer, firstName], (err, results, fields) => {
+    if (err) {
+      res.send(false);
+    } else {
+      res.send(true);
+    }
+  });
+}
 
 app.listen(config.port, () => {
   console.log(`server listening ${config.port}...`);
