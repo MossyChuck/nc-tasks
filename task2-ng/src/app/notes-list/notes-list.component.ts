e663dc253a0da81cff766967d2aa24b55a0fd9f0
MossyChuck <ipasha1975@gmail.com>
2018-04-16 04:08:11 +0300

    adding note

--- task2-ng/src/app/notes-list/notes-list.component.ts
+++ task2-ng/src/app/notes-list/notes-list.component.ts
@@ -11,6 +11,9 @@ import {Note} from "../notes-model/Note";
 export class NotesListComponent implements OnInit {
 
   notes: Note[] = [];
+  isAdding: boolean = false;
+  newNote: Note = new Note();
+  tagFilter: string = '';
 
   constructor(private notesService: NotesService) {
   }
@@ -19,6 +22,38 @@ export class NotesListComponent implements OnInit {
     this.notesService.getNotes().subscribe((notes) => {
       this.notes = notes;
     })
+  }
+
+  checkTags(){
+    let startIndex = 0;
+    this.newNote.tags = [];
+    while(1){
+      let start = this.newNote.text.indexOf('#', startIndex);
+      if(start === -1){
+        return;
+      }
+      let end = this.newNote.text.indexOf(' ',start) === -1? this.newNote.text.length: this.newNote.text.indexOf(' ',start);
+      let tag = this.newNote.text.substring(start,end);
+      this.newNote.tags.push(tag);
+      console.log(tag);
+      startIndex = end;
+    }
+  }
+
+  startAdding(){
+    this.isAdding = !this.isAdding;
+  }
+
+  save(){
+    this.notesService.addNote(this.newNote.text,this.newNote.tags).subscribe((note)=>{
+      this.notes.push(note);
+      this.cancel();
+    });
+  }
+
+  cancel(){
+    this.isAdding = !this.isAdding;
+    this.newNote = new Note();
   }
 
   ngOnInit() {
