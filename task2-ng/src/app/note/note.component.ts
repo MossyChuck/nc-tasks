088e9aac12da156ff3147f4f25606ee42a6f1481
MossyChuck <ipasha1975@gmail.com>
2018-04-16 03:09:06 +0300

    highlighting tags

--- task2-ng/src/app/note/note.component.ts
+++ task2-ng/src/app/note/note.component.ts
@@ -11,14 +11,31 @@ export class NoteComponent implements OnInit {
 
   @Input()
   note: Note;
+  noteHtmlText: string;
   isEditing: boolean = false;
   tempNote: Note;
 
   edit(){
     this.isEditing = !this.isEditing;
     this.tempNote = JSON.parse(JSON.stringify(this.note));
-    this.note.tags.push('#qwe');
+    this.highligthTags();
     console.log(`Note ${this.note.id} isEditing is ${this.isEditing}`);
+  }
+
+  highligthTags(){
+    let startIndex = 0;
+    this.noteHtmlText = this.note.text+' ';
+    while(1){
+      let start = this.noteHtmlText.indexOf('#', startIndex);
+      if(start === -1){
+        return;
+      }
+      let end = this.noteHtmlText.indexOf(' ',start) === -1? this.noteHtmlText.length: this.noteHtmlText.indexOf(' ',start);
+      let subs = this.noteHtmlText.substring(start,end)+' ';
+      console.log(subs);
+      this.noteHtmlText = this.noteHtmlText.replace(subs,`<span class="highlited" style="color: red">${subs.replace(' ','')}</span> `);
+      startIndex = this.noteHtmlText.lastIndexOf('</span>');
+    }
   }
 
   save(){
